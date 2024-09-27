dac4f8d5347884acba32105b28340d9c7749cab4
CodeBridgeGenerator <kana@codebridge.my>
2024-09-27 01:54:08 +0000

    CodeBridge commit

---
+++ react-frontend/src/components/NotifyPage/notifyFakerFactory.js
@@ -0,0 +1,18 @@
+
+import { faker } from "@faker-js/faker";
+export default (user,count) => {
+    let data = [];
+    for (let i = 0; i < count; i++) {
+        const fake = {
+tajuk: faker.lorem.sentence(1),
+venue: faker.lorem.sentence(1),
+tarikh: faker.lorem.sentence(1),
+pengajur: faker.lorem.sentence(1),
+
+updatedBy: user._id,
+createdBy: user._id
+        };
+        data = [...data, fake];
+    }
+    return data;
+};
